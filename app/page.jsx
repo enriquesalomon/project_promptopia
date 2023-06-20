1f2e82f9d59670e2b5d8d158cc12e12598894a3d
enriquesalomon <king92salomon@gmail.com>
2023-06-20 17:07:13 +0800

    adding a comment in page.jsx using branch dev1

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,25 @@
-import Feed from '@components/Feed';
+import Feed from "@components/Feed";
 // import React from 'react'
 
 function Home() {
   return (
-    <section className='w-full flex-center flex-col'>
-      <h1 className='head_text text-center'>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
         Shout-out for Friends & Share
-        <br className='max-md:hidden' />
-        <span className='orange_gradient text-center'> Friend Shout-out</span>
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+          {" "}
+          Friend Shout-out FOR BRANCH SAMPLE DEV 1
+        </span>
       </h1>
-      <p className='desc text-center'>
-        Friend Shout-out is an open-source prompting tool for individual looking for friends, create and share
+      <p className="desc text-center">
+        Friend Shout-out is an open-source prompting tool for individual looking
+        for friends, create and share
       </p>
 
       <Feed />
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
